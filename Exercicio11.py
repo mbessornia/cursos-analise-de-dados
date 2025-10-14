546bd740c8ee380a399109f78008112dc7a74d74
Mateus Bessornia <150955006+mbessornia@users.noreply.github.com>
2025-10-13 23:38:42 -0300

    Base - Estruturas condicionais

---
+++ Exercicio11.py
@@ -0,0 +1,19 @@
+l1 = float(input(f'Informe o primeiro lado do triângulo: '))
+l2 = float(input(f'Informe o segundo lado do triângulo: '))
+l3 = float(input(f'Informe o terceiro lado do triângulo: '))
+
+
+if (l1 + l2) < l3:
+    print('Não é possível formar um triângulo.')
+elif (l2 + l3) < l1:
+    print('Não é possível formar um triângulo.')
+elif (l1 + l3) < l2:
+    print('Não é possível formar um triângulo.')
+else:
+    if l1 == l2 == l3:
+        print('É possível formar um triângulo equilátero!')
+    elif l1 == l2 or l2 == l3 or l1 == l3:
+        print('É possível formar um triângulo Isósceles!')
+    elif l1 != l2 != l3:
+        print('É possível formar um triângulo Escaleno!')
+
